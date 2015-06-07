4e6e58e40bcd959208d890ec581fbde75034b60b
Bob Lail <bob.lail@cph.org>
2015-06-07 11:55:56 -0500

    [skip] Moved `/reports/by_user/...` to `/reports/weekly/by_user/...` (1m)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Houston::Reports::Engine.routes.draw do
   
   get "star", to: "reports#star"
-  get "by_user/:nickname", to: "reports#user_report"
-  get "by_user/:nickname/star.xlsx", to: "reports#user_star_report"
-  
 
   get "sprint/dashboard", to: "reports#sprint"
   get "sprint/:id/dashboard", to: "reports#sprint"
+
+  get "weekly/by_user/:nickname", to: "reports#user_report"
+  get "weekly/by_user/:nickname/star.xlsx", to: "reports#user_star_report"
 end
