89bece089db91682bb9021f075cf7ca3b2d4cbe2
Bob Lail <bob.lail@cph.org>
2015-10-10 11:25:58 -0500

    [skip] Namespaced routes (1m)

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 Houston::Reports::Engine.routes.draw do
 
-  # Shows who has entered their Star and Empower time over the last two weeks
-  get "star/dashboard", to: "reports#star"
+  scope "reports" do
+    # Shows who has entered their Star and Empower time over the last two weeks
+    get "star/dashboard", to: "reports#star"
 
-  # Modified Sprint dashboard that shows GoalKeeper goal progress as well
-  get "sprint/dashboard", to: "reports#sprint"
-  get "sprint/:id/dashboard", to: "reports#sprint"
+    # Modified Sprint dashboard that shows GoalKeeper goal progress as well
+    get "sprint/dashboard", to: "reports#sprint"
+    get "sprint/:id/dashboard", to: "reports#sprint"
 
-  # Alerts, historical
-  get "alerts", to: "alerts#index"
+    # Alerts, historical
+    get "alerts", to: "alerts#index"
 
-  # Scorecards
-  get "weekly/by_user/:nickname", to: "reports#user_report"
+    # Scorecards
+    get "weekly/by_user/:nickname", to: "reports#user_report"
 
-  # Special Star report download (to remove)
-  get "weekly/by_user/:nickname/star.xlsx", to: "reports#user_star_report"
+    # Special Star report download (to remove)
+    get "weekly/by_user/:nickname/star.xlsx", to: "reports#user_star_report"
 
-  # Sprint and Alerts details for each week
-  get "weekly", to: "reports#weekly_report", as: :weekly_report
+    # Sprint and Alerts details for each week
+    get "weekly", to: "reports#weekly_report", as: :weekly_report
+  end
 
 end
